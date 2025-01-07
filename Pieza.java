c348fff98ad635aeb69f7178fea6a255fcfddccb
Alberto-Ruiz-Blasco <albruibla@alu.edu.gva.es>
2025-01-07 08:21:28 +0100

    Add files via upload

--- Pieza.java
+++ Pieza.java
@@ -23,6 +23,13 @@ public class Pieza {
         }
     }
 
+    public Pieza() {
+        this.tipo = ' ';
+        this.color = false;
+        this.posI = 0;
+        this.posJ = 0;
+    }
+
     public char getTipo() {
         return tipo;
     }
@@ -33,6 +40,14 @@ public class Pieza {
 
     public boolean isColor() {
         return color;
+    }
+
+    public void getColor() {
+        if (!this.color) {
+            System.out.println("Blanco");
+        } else {
+            System.out.println("Negro");
+        }
     }
 
     public void setColor(boolean color) {
@@ -55,7 +70,7 @@ public class Pieza {
         this.posJ = posJ;
     }
 
-    public int[][] comprobarPosicion(int[] movimientosFila, int[] movimientosColumna, int[][] posibilidades){
+    public int[][] comprobarPosicion(int[] movimientosFila, int[] movimientosColumna, int[][] posibilidades) {
         int contador = 0;
 
         for (int i = 0; i < movimientosFila.length; i++) {
@@ -78,14 +93,57 @@ public class Pieza {
         int[] movimientosFila = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] movimientosColumna = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-        return comprobarPosicion(movimientosFila,movimientosColumna,posibilidades);
+        return comprobarPosicion(movimientosFila, movimientosColumna, posibilidades);
     }
 
-    public int[][] movimientoAlfil(){
+    public int[][] movimientoAlfil() {
         int[][] posibilidades = new int[13][2];
-        int[] movimientosFila = {-1,-1,1,1,-2,-2,2,2,-3,-3,3,3,-4,-4,4,4,-5,-5,5,5,-6,-6,6,6,-7,-7,7,7};
-        int[] movimientosColumna = {-1,1,-1,1,-2,2,-2,2,-3,3,-3,3,-4,4,-4,4,-5,5,-5,5,-6,6,-6,6,-7,7,-7,7};
+        int[] movimientosFila = {-1, -1, 1, 1, -2, -2, 2, 2, -3, -3, 3, 3, -4, -4, 4, 4, -5, -5, 5, 5, -6, -6, 6, 6, -7, -7, 7, 7};
+        int[] movimientosColumna = {-1, 1, -1, 1, -2, 2, -2, 2, -3, 3, -3, 3, -4, 4, -4, 4, -5, 5, -5, 5, -6, 6, -6, 6, -7, 7, -7, 7};
 
-        return comprobarPosicion(movimientosFila,movimientosColumna,posibilidades);
+        return comprobarPosicion(movimientosFila, movimientosColumna, posibilidades);
     }
+
+
+    public int[][] movimientoPeon() {
+        // Inicializamos el arreglo de posibles posiciones
+        int[][] posibilidades = new int[2][2];  // Ahora tiene 3 posibles movimientos: una casilla, dos casillas, captura en diagonal.
+        int[] movimientosFila;  // Dirección de filas (el peón solo se mueve hacia abajo, en la parte superior y diagonal)
+        int[] movimientosColumna; // Movimiento en las columnas
+
+
+        // Movimiento para el peón negro
+        if (this.color == false) {  // Peón negro
+            if (this.posI == 7) {  // Si el peón está en la fila 7, puede moverse 1 o 2 casillas hacia adelante
+                movimientosFila = new int[]{-1, -2};  // Movimiento hacia arriba
+                movimientosColumna = new int[]{0, 0};  // La columna no cambia
+            } else {  // Si no está en la fila 7, solo puede moverse 1 casilla hacia adelante
+                movimientosFila = new int[]{-1};
+                movimientosColumna = new int[]{0};
+            }
+        }
+        // Movimiento para el peón blanco
+        else {  // Peón blanco
+            if (this.posI == 2) {  // Si el peón está en la fila 2, puede moverse 1 o 2 casillas hacia adelante
+                movimientosFila = new int[]{1, 2};  // Movimiento hacia abajo
+                movimientosColumna = new int[]{0, 0};  // La columna no cambia
+            } else {  // Si no está en la fila 2, solo puede moverse 1 casilla hacia adelante
+                movimientosFila = new int[]{1};
+                movimientosColumna = new int[]{0};
+            }
+        }
+
+        // Verificación de las posiciones usando el método comprobarPosicion
+
+        return comprobarPosicion(movimientosFila, movimientosColumna, posibilidades);
+    }
+
+    public int[][] movimientoCaballo() {
+        int[][] posibilidades = new int[8][2];
+        int[] movimientosFila = {2,-2,-1,2,-2,1,-1,1};
+        int[] movimientosColumna = {-1,1,-2,1,-1,2,2,-2};
+
+        return comprobarPosicion(movimientosFila, movimientosColumna, posibilidades);
+    }
+
 }
